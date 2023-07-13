cd8ce50ece9707ce87a33b5f41913cec919ab128
SimoLogic <mohamedelheimer0@gmail.com>
2023-07-13 14:59:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: A C program that prints with put function
- *
- * Return Always 0 (Success)
-*/
-
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzze");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
