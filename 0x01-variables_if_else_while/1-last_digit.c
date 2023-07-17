43c9d5f68ee108d6677e9f72e2a916957c7b8978
SimoLogic <mohamedelheimer0@gmail.com>
2023-07-17 15:59:26 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main - Prints a random number and each time it is executed
  *        print the last digit of the number stored in the variable.
@@ -16,17 +17,17 @@
 
 	if ((n % 10) > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n",
+		printf("Last digit of i% is i% and is greater than 5\n",
 			n, n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+		printf("Last digit of %i is %i and is less than 6 and not 0\n",
 			n, n % 10);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is 0\n",
+		printf("Last digit of %i is %i and is 0\n",
 			n, n % 10);
 	}
 
