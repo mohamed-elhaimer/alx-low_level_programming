d2075ff2313e95cea7cfe7d5a3860c1020c7d003
SimoLogic <mohamedelheimer0@gmail.com>
2023-07-20 11:45:51 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/*
+ * File: main.h
+ * Auth: SimoLogic
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
 int _putchar(char c);
 int _isupper(int c);
 
