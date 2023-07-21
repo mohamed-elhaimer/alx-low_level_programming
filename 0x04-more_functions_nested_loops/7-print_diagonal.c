c96abb48bc932798d87fa63e43c857ddb60f146d
SimoLogic <mohamedelheimer0@gmail.com>
2023-07-21 10:15:33 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line using the \ character.
+ * @n: The number of \ characters to be printed.
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		_putchar('\');
+	}
+	_putchar('\n');
+}
