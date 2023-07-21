0c612b91d2af2753ab983e092a2ac84905e30632
SimoLogic <mohamedelheimer0@gmail.com>
2023-07-21 12:20:09 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,10 +6,12 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, c;
 
 	if (n > 0)
 	{
+		for (c = 1; c <= n; c++)
+			_putchar(' ');
 		for (i = 0; i < n; i++)
 		_putchar('\\');
 		_putchar('\n');
