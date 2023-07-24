bcc0492208aabb534951d6ba43cb7a893789b298
SimoLogic <mohamedelheimer0@gmail.com>
2023-07-24 16:47:56 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: The length of @str.
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int len;
+	int i;
 
-	len = 0;
-	while (*s != '\0')
+	i = 0;
+
+	while (s[i] != '\0')
 	{
-		_putchar(*s);
-		s++;
-		len++;
+		i++;
 	}
-	return (len);
+
+	return (i);
 }
