389a4c9396038e349444940f28232fca0cf03f56
SimoLogic <mohamedelheimer0@gmail.com>
2023-07-24 10:51:46 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ * Auth: SimoLogic
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x05. C - Pointers, arrays and strings.
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
