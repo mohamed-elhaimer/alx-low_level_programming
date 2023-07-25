7090f9ab3c54b3deb5421064e7bac98ecb25feed
SimoLogic <mohamedelheimer0@gmail.com>
2023-07-25 12:35:20 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,14 +4,26 @@
  * rev_string - reverses a string
  * @s: string to be printed
  */
+
 void rev_string(char *s)
 {
+	char tmp;
+
 	int i, j, len;
 
-	for (i = 0; i != '\0'; i++)
-		_putchar(s[i]);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 	len = i;
+	i = len - 1;
+	for (j = 0; j <= len / 2; j++)
+	{
+		tmp = s[i];
+		s[i] = s[j];
+		s[j] = tmp;
+		i--;
+	}
 	_putchar('\n');
-	for (j = len; i >= 0; i--)
-		_putchar(s[j]);
 }
