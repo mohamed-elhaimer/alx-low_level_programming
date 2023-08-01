3fd04d298ac8aa71e6a9613dd7f1243ebee8cc5b
Yahaya <shopoldbooks@gmail.com>
2023-08-01 19:18:29 +0100

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,17 @@
+#include "main.h"
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (s + i);
+}
