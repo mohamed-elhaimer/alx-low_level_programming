21b7a5ce78d0477e62ada502bcba1abdcabe1c7a
Yahaya <shopoldbooks@gmail.com>
2023-08-01 15:33:16 +0100

    3tasks

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ * Auth: SimoLogic
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06. C - More pointers, arrays and strings.
+ */
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+#endif
