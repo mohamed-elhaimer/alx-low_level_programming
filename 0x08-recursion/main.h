15533eb2926c5875377e5d51e7db580f2bf4d351
Yahaya <shopoldbooks@gmail.com>
2023-08-02 16:40:01 +0100

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ * Auth: SimoLogic
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06. C - More pointers, arrays and strings.
+ */
+int _putchar(char c);
+void _puts_recursion(char *s);
+#endif
