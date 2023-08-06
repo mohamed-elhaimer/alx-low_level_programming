000a77c8e73b26cc43ed2c0c2b7d6facf1127185
SimoLogic <mohamedelheimer0@gmail.com>
2023-08-06 15:15:34 +0100

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - print multiples of two numbers, followed by a new line
+ * @argv: pointer to an array string passed to programe
+ * @argc: number of commande line argument
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int res;
+
+	if (argc == 3)
+	{
+		res = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", res);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
