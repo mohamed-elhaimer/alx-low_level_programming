f5ff9f76aa0e1b2a7175124f077cd0f8f51d4c66
SimoLogic <mohamedelheimer0@gmail.com>
2023-08-08 15:59:29 +0100

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * _strdup - copies the string given as parameter
+ * @str: string to duplicate
+ * Return: Pointer that copied string
+*/
+char *_strdup(char *str)
+{
+	int i;
+	char *ptr;
+
+	if (str == NULL)
+	return (0);
+	ptr = (char *)malloc(strlen(str) * sizeof(char));
+	if (ptr == 0)
+	return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
+
