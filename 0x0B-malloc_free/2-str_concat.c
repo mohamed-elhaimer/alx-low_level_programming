57ba4bd90fc1e050382910cfac1c9e39cc3ac055
SimoLogic <mohamedelheimer0@gmail.com>
2023-08-08 18:17:22 +0100

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,8 @@ char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
 
+	if (s1 == NULL || s2 == NULL)
+	return (NULL);
 	ptr = (char *)malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 	if (ptr == 0)
 	return (NULL);
