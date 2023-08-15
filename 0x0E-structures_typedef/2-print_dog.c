cc21d0bc40fcf84899ea58e3408b0289ecce6b63
SimoLogic <mohamedelheimer0@gmail.com>
2023-08-15 19:18:32 +0100

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print attributes dog struct
+ * @d: a dog structur
+ * Return: Nothing
+*/
+void print_dog(struct dog *d)
+{
+	if (d == 0)
+	return;
+	if ((*d).name == 0)
+	printf("Name: Nil %s\n", d->name);
+	else
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if ((*d).owner == 0)
+	printf("Owner: Nil %s\n", d->owner);
+	printf("Owner: %s\n", d->owner);
+}
