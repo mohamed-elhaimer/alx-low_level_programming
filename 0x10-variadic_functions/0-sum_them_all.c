18628f7b77d5e5bbf91374661413e300bab5738c
SimoLogic <mohamedelheimer0@gmail.com>
2023-08-18 14:21:13 +0100

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,14 +9,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
+	unsigned int i;
+	int sum = 0;
 
 	if (n == 0)
 	return (0);
 	va_start(args, n);
 
-	int sum = 0;
-
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(args, int);
 	}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,8 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
 
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
