e7a4fff64a1d29fd9cdfc11709cc40332b9674b5
Mohamed El haimer <mohamedelheimer0.com>
2023-08-25 14:35:36 +0100

    0x10-variadic_functions

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: name of list
+ * Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	int len = 0;
+
+	while (h != NULL)
+	{
+		if ((*h).str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("%d,%s", (*h).len, (*h).str);
+		}
+		h = (*h).next;
+	}
+	return (len);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+tasks about linked list
