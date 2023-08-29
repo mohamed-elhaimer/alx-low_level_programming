6dd9c7bda62f117a25f984af4169b6ac19edaf57
Mohamed El haimer <mohamedelheimer0.com>
2023-08-29 11:06:45 +0100

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,10 +8,28 @@ void free_listint(listint_t *head)
 {
 	listint_t *ptr;
 
-	ptr = head;
-	while (ptr->next != NULL)
+	while (head != NULL)
 	{
-		ptr = ptr->next;
+		ptr = head->next;
+		free(head);
+		head = ptr;
 	}
-	free(ptr);
+}
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    free_listint(head);
+    head = NULL;
+    return (0);
 }
