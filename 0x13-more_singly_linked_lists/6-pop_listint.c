7464576aa37ecc4aa22d1b4cd537c7a3a1cdd426
Mohamed El haimer <mohamedelheimer0.com>
2023-08-29 16:08:32 +0100

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - delete the head node
+ * @head: list
+ * Return: data of the node head path
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+	int num;
+
+	if (head == NULL)
+	return (0);
+	num = (*head)->n;
+	ptr = *head;
+	*head = (*head)->next;
+	free(ptr);
+	ptr = NULL;
+	return (num);
+}
