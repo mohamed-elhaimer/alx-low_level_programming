e02e338c67a233f7a9e21fa472486e2955813123
Mohamed El haimer <mohamedelheimer0.com>
2023-09-06 13:56:34 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,11 +16,12 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		l = strlen(text_content);
-	}
+
 		fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 		wr = write(fp, text_content, l);
 		if (wr == -1 || fp == -1)
 		return (-1);
+	}
 	close(fp);
 	return (1);
 }
