0dbdbfadc05abce88b19ab5f4be1bcd92a837a2a
mohamed-Logic <mohamedelheimer0@gmail.com>
2023-09-08 10:57:22 +0100

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(98);
+		exit(97);
 	}
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 == -1)
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
 		if (write(file2, buff, n) != n || file2 == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(98);
+			exit(99);
 		}
 	}
 	if (n == -1)
@@ -39,12 +39,12 @@ int main(int argc, char **argv)
 	if (close(file1) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1);
-		exit(98);
+		exit(100);
 	}
 	if (close(file2) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
-		exit(98);
+		exit(100);
 	}
 	return (0);
 }
