f37d7cab00e9f58d3e400940f0c2193211425470
Simo <simo@Simos-Laptop.local>
2023-10-12 12:34:11 +0100

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,6 +10,8 @@ size_t print_dlistint(const dlistint_t *h)
 
     if(h == NULL)
     return (0);
+    while (h->prev != NULL)
+		h = h->prev;
     while (h != NULL)
     {
         printf("%d\n", h->n);
