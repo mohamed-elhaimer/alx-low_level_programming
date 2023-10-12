ba38eb6c0a4bc375ecf5a00207dff39ec86ce893
SimoLogic <mohamedelheimer0@gmail.com>
2023-10-12 14:39:20 +0100

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,12 +8,10 @@ size_t print_dlistint(const dlistint_t *h)
 {
     size_t l;
     
-    l = 0;
-    while (h != NULL)
+    for (l = 0; h != NULL; l++)
     {
         printf("%d\n", h->n);
         h = h->next;
-        l++;
     }
     return (l);
 }
