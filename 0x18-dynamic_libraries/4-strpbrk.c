7c1a1e170e856d088418db1f7da04529bbde7fab
SimoLogic <mohamedelheimer0@gmail.com>
2023-10-16 14:28:33 +0100

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes
+ *
+ * @s:first occurrence in the string
+ *
+ * @accept: matches one of the bytes, or @NULL if no such byte
+ *
+ * Return: a pointer to the byte
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+		}
+	}
+	return (0);
+}
