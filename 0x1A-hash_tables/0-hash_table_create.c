be92d0f3b0f808b6b136ccae3c0d6cfc33b5c801
SimoLogiC <mohamedelheimer0@gmail.com>
2023-11-03 14:38:44 +0100

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,13 +7,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *Table;
-	int i;
+	unsigned long int i;
 
-	Table = (hash_table_t *) malloc(size * sizeof(hash_table_t));
+	Table = (hash_table_t *) malloc(sizeof(hash_table_t));
 	if (Table == NULL)
-	return (NULL);
+		return (NULL);
 	Table->size = size;
-	Table->array = (hash_node_t **) calloc(Table->size,  sizeof(hash_node_t *));
+	Table->array = (hash_node_t **) malloc(size * sizeof(hash_node_t *));
+	if (Table->array == NULL)
+	{
+		free(Table);
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		Table->array[i] = NULL;
