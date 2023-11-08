47ff81355ac35478876d288fb1a574e8d2da9968
SimoLogiC <mohamedelheimer0@gmail.com>
2023-11-08 23:28:23 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || *key == '\n' || *value == '\n')
 	return (0);
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	item = ht->array[index];
 	if (item == NULL)
 	{
