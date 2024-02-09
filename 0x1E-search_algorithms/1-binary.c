3343cbfc1b4d9721abc76f8b1660cbdd4def92cb
SimoLogic <mohamedelheimer0@gmail.com>
2024-02-09 20:59:49 +0100

    1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,39 +1,61 @@
 #include "search_algos.h"
+
 /**
- * binary_search - function that searches for a value in a sorted array
- * @array: is a pointer to the first element of the array to search in
- * @size:  is the number of elements in array
- * @value:  is the value to search for
- * Return: return the index where value is located
-*/
-int binary_search(int *array, size_t size, int value)
+ * recursive_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int recursive_search(int *array, size_t size, int value)
 {
-	size_t low, height, middle, i;
-	size_t target;
+	size_t half = size / 2;
+	size_t i;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
-	low = 0;
-	height = size - 1;
-	while (low <= height)
-	{
-		middle = low + (height - low) / 2;
-		target = array[middle];
+	printf("Searching in array");
 
-		printf("Searching in array");
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
 
-		for (i = low; i <= height; i++)
-			printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+	printf("\n");
 
-		printf("\n");
-		if ((size_t)target == (size_t)value)
-			return (middle);
-		else if ((size_t)value > (size_t)target)
-			low = middle + 1;
-		else
-			height = middle - 1;
+	if (half && size % 2 == 0)
+		half--;
 
-	}
-	return (-1);
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int index;
+
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
 }
--- 0x1E-search_algorithms/1-main.c
+++ 0x1E-search_algorithms/1-main.c
@@ -1,6 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
+/**
+ * binary_search - function that searches for a value in a sorted array
+ * @array: is a pointer to the first element of the array to search in
+ * @size:  is the number of elements in array
+ * @value:  is the value to search for
+ * Return: return the index where value is located
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	size_t low, height, middle, i;
+	size_t target;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	low = 0;
+	height = size - 1;
+	while (low <= height)
+	{
+		middle = low + (height - low) / 2;
+		target = array[middle];
+
+		printf("Searching in array");
+
+		for (i = low; i <= height; i++)
+			printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+		printf("\n");
+		if ((size_t)target == (size_t)value)
+			return (middle);
+		else if ((size_t)value > (size_t)target)
+			low = middle + 1;
+		else
+			height = middle - 1;
+
+	}
+	return (-1);
+}
 
 /**
  * main - Entry point
